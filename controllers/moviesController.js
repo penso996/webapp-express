08da435593e32e54127449d5cf9aafc293d0d9de
penso996 <penso.andrea@outlook.it>
2025-03-10 18:56:05 +0100

    working on controller

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -1,17 +1,28 @@
-// Importing data
-const filmsDb = require("../data/moviesDatabase");
+// Importing movies data from database
+const moviesDatabase = require("../data/moviesDatabase");
 
 // Index function
 function index(req, res) {
+
     // SQL query to show all movies
     const sql = `SELECT
-	    movies.id,
-	    movies.title
-    FROM moviess`;
+        movies.id,
+        movies.title
+    FROM movies`;
 
-    // Execute query
-    filmsDb.query(sql, (err, movies) => {
-        if (err) return res.status(500).json({ error: "Database query failed" });
+    // Execute SQL query
+    moviesDatabase.query(sql, (err, movies) => {
+        if (err) {
+            console.error("Database query failed:", err);
+            return res.status(500).json({ error: "Database query failed" });
+        }
+
+        // If no movies are found
+        if (movies.length === 0) {
+            return res.status(404).json({ message: "No movies found" });
+        }
+
+        // Return movies data as JSON
         res.json(movies);
     });
 }
@@ -19,6 +30,35 @@ function index(req, res) {
 // Show function
 function show(req, res) {
 
+    // Read ID from URL
+    const id = parseInt(req.params.id);
+
+    // SQL query to show the movie with the given ID
+    const sql = `SELECT
+        movies.id,
+        movies.title,
+        movies.director,
+        movies.genre,
+        movies.release_year,
+        movies.abstract
+    FROM movies
+    WHERE movies.id = ?`;
+
+    // Execute SQL Query                     
+    moviesDatabase.query(sql, [id], (err, movies) => {
+        if (err) {
+            console.error("Database query failed:", err);
+            return res.status(500).json({ error: "Failed to retrieve movie data" });
+        }
+
+        // If no movie is found
+        if (movies.length === 0) {
+            return res.status(404).json({ error: "Movie not found" });
+        }
+
+        // Return movie data as JSON
+        res.json(movies[0]);
+    });
 }
 
 // Store function
